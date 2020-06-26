b36c0adbcbe32f56f71d265a2c047eba0ce1a2ad
romanornr <romanornr@gmail.com>
2020-06-26 23:21:27 +0200

    Use FTX API

--- futures/expired.go
+++ futures/expired.go
@@ -1,8 +1,130 @@
 package futures
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+//type ExpiredFutures struct {
+//	Ticker string
+//	Name string
+//	ExpiryDate string
+//	ExpirationPrice float64
+//}
+
+type ExpiredFuturesResponse struct {
+	Result []struct {
+		Ask                   interface{} `json:"ask"`
+		Bid                   interface{} `json:"bid"`
+		Description           string      `json:"description"`
+		Enabled               bool        `json:"enabled"`
+		Expired               bool        `json:"expired"`
+		Expiry                time.Time   `json:"expiry"`
+		ExpiryDescription     string      `json:"expiryDescription"`
+		Group                 string      `json:"group"`
+		ImfFactor             float64     `json:"imfFactor"`
+		Index                 float64     `json:"index"`
+		Last                  float64     `json:"last"`
+		LowerBound            float64     `json:"lowerBound"`
+		MarginPrice           float64     `json:"marginPrice"`
+		Mark                  float64     `json:"mark"`
+		MoveStart             interface{} `json:"moveStart"`
+		Name                  string      `json:"name"`
+		Perpetual             bool        `json:"perpetual"`
+		PositionLimitWeight   float64     `json:"positionLimitWeight"`
+		PostOnly              bool        `json:"postOnly"`
+		PriceIncrement        float64     `json:"priceIncrement"`
+		SizeIncrement         float64     `json:"sizeIncrement"`
+		Type                  string      `json:"type"`
+		Underlying            string      `json:"underlying"`
+		UnderlyingDescription string      `json:"underlyingDescription"`
+		UpperBound            float64     `json:"upperBound"`
+	} `json:"result"`
+	Success bool `json:"success"`
+}
+
 type ExpiredFutures struct {
-	Ticker string
-	Name string
-	ExpiryDate string
-	ExpirationPrice float64
+	ExpiredFutures []ExpiredFuture
+}
+
+type ExpiredFuture struct {
+	Ask                   interface{} `json:"ask"`
+	Bid                   interface{} `json:"bid"`
+	Description           string      `json:"description"`
+	Enabled               bool        `json:"enabled"`
+	Expired               bool        `json:"expired"`
+	Expiry                time.Time   `json:"expiry"`
+	ExpiryDescription     string      `json:"expiryDescription"`
+	Group                 string      `json:"group"`
+	ImfFactor             float64     `json:"imfFactor"`
+	Index                 float64     `json:"index"`
+	Last                  float64     `json:"last"`
+	LowerBound            float64     `json:"lowerBound"`
+	MarginPrice           float64     `json:"marginPrice"`
+	Mark                  float64     `json:"mark"`
+	MoveStart             interface{} `json:"moveStart"`
+	Name                  string      `json:"name"`
+	Perpetual             bool        `json:"perpetual"`
+	PositionLimitWeight   float64     `json:"positionLimitWeight"`
+	PostOnly              bool        `json:"postOnly"`
+	PriceIncrement        float64     `json:"priceIncrement"`
+	SizeIncrement         float64     `json:"sizeIncrement"`
+	Type                  string      `json:"type"`
+	Underlying            string      `json:"underlying"`
+	UnderlyingDescription string      `json:"underlyingDescription"`
+	UpperBound            float64     `json:"upperBound"`
+}
+
+func GetExpiredFutures() ExpiredFuturesResponse {
+	apiEndpoint, err := url.Parse("https://ftx.com/api/expired_futures")
+	if err != nil {
+		logrus.Fatalf("Could not parse expired futures api endpoint: %s\n", err)
+	}
+
+	client := http.Client{
+		Transport:     nil,
+		CheckRedirect: nil,
+		Jar:           nil,
+		Timeout:       time.Second * 15,
+	}
+	req, err := http.NewRequest(http.MethodGet, apiEndpoint.String(), nil)
+	if err != nil {
+		logrus.Fatalf("Could not make a new request to %s\n", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Fatalf("Client.Do failed for API endpoint %s %s\n", apiEndpoint.String(), err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Fatalf("Could not read body response from API endpoint %s %s\n", apiEndpoint.String(), err)
+	}
+
+	expiredFutures := ExpiredFuturesResponse{}
+	err = json.Unmarshal(body, &expiredFutures)
+	if err != nil {
+		logrus.Fatalf("Failed to unmarshal ExpiredFutures: %s\n", err)
+	}
+
+	return expiredFutures
+}
+
+func (expiredFuturesResp ExpiredFuturesResponse) DailyMOVE() {
+	response := expiredFuturesResp.Result
+	expiredFutures := new(ExpiredFutures)
+
+	for _, expiredFuture := range response {
+		if expiredFuture.Type == "move" && expiredFuture.Underlying == "BTC" {
+			expiredFutures.ExpiredFutures = append(expiredFutures.ExpiredFutures, expiredFuture)
+		}
+	}
+	fmt.Println(expiredFutures)
 }
---
+++ futures/move.go
@@ -0,0 +1,21 @@
+package futures
+
+type MOVE struct {
+	ExpiredFutures
+	//Description string
+	//Group string
+	//Expired bool
+	//Day time.Weekday
+	//Time string
+	//Last float64
+	//UpperBound float64
+	//Type string
+	//underlying string
+}
+
+//type Statistics struct {
+//	Static []Statistic
+//	Day time.Weekday
+//	AverageExpirationPrice float64
+//}
+
