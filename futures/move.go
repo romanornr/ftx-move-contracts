e634f11bb68a4d0157edffea87d6e5535a5723ef
romanornr <romanornr@gmail.com>
2020-06-27 23:32:23 +0200

    Cleanup

--- futures/move.go
+++ futures/move.go
@@ -3,16 +3,4 @@ package futures
 type MOVEContracts struct {
 	Expired                []ExpiredFuture
 	AverageExpirationPrice float64
-	//Day time.Weekday
-	//Time string
 }
-
-//type MOVEContracts struct {
-//	MOVEContract []MOVEContract
-//}
-
-//type Statistics struct {
-//	Static []Statistic
-//	Day time.Weekday
-//	AverageExpirationPrice float64
-//}
